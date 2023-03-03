daf95998649041be1c408fb35cef773e852d5a0e
Artur <arthurlomakin11@gmail.com>
2023-03-03 20:25:23 +0200

    Try to select Files inside new Comment builder Gives error: System.NotImplementedException: The method or operation is not implemented.

--- Controllers/CommentTreeController.cs
+++ Controllers/CommentTreeController.cs
@@ -1,8 +1,12 @@
-﻿using LinqToDB;
+﻿using System.Linq;
+using System.Text.Json.Serialization;
+using LinqToDB;
 using LinqToDB.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TestAppDzenCode.Controllers.Extensions;
 using TestAppDzenCode.Data;
+
 using File = TestAppDzenCode.Data.File;
 
 namespace TestAppDzenCode.Controllers;
@@ -28,28 +32,59 @@ public class CommentTreeController : ControllerBase
         var commentsHierarchyCteAns = _commentsLinq2DbContext.GetCte<Comment>(commentHierarchy =>
         {
             return _commentsLinq2DbContext.GetTable<Comment>().Where(c => c.ParentId == Id)
+                .Include(c => c.Comments)
                 .Concat
                 (
                     from c in _commentsLinq2DbContext.GetTable<Comment>()
                     from c2 in commentHierarchy.InnerJoin(eh => c.Id == eh.ParentId)
-                    select c
+                    select new Comment
+                    {
+                        Id = c.Id,
+                        ParentId = c.ParentId,
+                        Email = c.Email,
+                        UserName = c.UserName,
+                        Text = c.Text,
+                        DateAdded = c.DateAdded,
+                        Files = new List<File> {
+                            (from f in _commentsLinq2DbContext.GetTable<File>()
+                                where f.CommentId == c.Id
+                                select f).First()
+                        }
+                    }
                 );
         });
         
         var commentsHierarchyCteDes = _commentsLinq2DbContext.GetCte<Comment>(commentHierarchy =>
         {
             return _commentsLinq2DbContext.GetTable<Comment>().Where(c => c.ParentId == Id)
+                .Include(c => c.Comments)
                 .Concat
                 (
                     from c in _commentsLinq2DbContext.GetTable<Comment>()
                     from c2 in commentHierarchy.InnerJoin(eh => c.ParentId == eh.Id)
-                    select c
+                    select new Comment
+                    {
+                        Id = c.Id,
+                        ParentId = c.ParentId,
+                        Email = c.Email,
+                        UserName = c.UserName,
+                        Text = c.Text,
+                        DateAdded = c.DateAdded,
+                        Files = new List<File> {
+                            (from f in _commentsLinq2DbContext.GetTable<File>()
+                            where f.CommentId == c.Id
+                            select f).First()
+                        }
+                    }
                 );
         });
 
         var result = commentsHierarchyCteAns.Union(commentsHierarchyCteDes);
 
-        var resultTree = result.ToList().GenerateTree(c => c.Id, c => c.ParentId);
+        var resultTree = result
+            .Include(c => c.Files)
+            .ToList()
+            .GenerateTree(c => c.Id, c => c.ParentId);
 
         return resultTree;
     }
